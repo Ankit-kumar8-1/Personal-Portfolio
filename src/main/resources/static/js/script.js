ccc9deea8ed23e9010374c300f6e9b308daffc21
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2025-11-17 07:12:26 +0530

    Updated index page and added Thymeleaf loader include

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,6 +1,11 @@
 // loading animation
 window.addEventListener("load", () => {
   setTimeout(() => {
-    document.querySelector(".loader_wrapper").style.opacity = "0";
+    const loader = document.querySelector(".loader_wrapper");
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
   }, 2000);
 });
