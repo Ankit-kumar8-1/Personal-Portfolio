a0eb6e0917381f20665bdd4c2769bdd8a0dccb95
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2025-11-17 20:27:55 +0530

    added final advanced hero image section with new CSS and animations

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -70,3 +70,17 @@ function typeRole() {
 }
 
 typeRole();
+
+const card = document.getElementById("tilt-card");
+
+card.addEventListener("mousemove", (e) => {
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left - rect.width / 2;
+  const y = e.clientY - rect.top - rect.height / 2;
+
+  card.style.transform = `rotateX(${-(y / 20)}deg) rotateY(${x / 20}deg)`;
+});
+
+card.addEventListener("mouseleave", () => {
+  card.style.transform = "rotateX(0deg) rotateY(0deg)";
+});
